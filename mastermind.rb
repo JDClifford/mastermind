ad4ab875fe760a03022912cb1b204a0dbbad7f76
John Clifford <johnclifford00@gmail.com>
2018-04-01 15:16:59 -0400

    Modified project file with additional methods. The game has a rough backbone.

--- mastermind.rb
+++ mastermind.rb
@@ -7,16 +7,63 @@ class Mastermind
 		@number_of_guesses = gets.chomp.to_i
 	end
 
-	def random_number_picker
-		x = 0
+	def random_number_picker #Picks the winning array
 		@winning_array = []
 		@string_length.times do
 			@winning_array.push(rand(6))
+			@winning_array.map! {|i| i = i.to_s}
 		end
-		@winning_array
+	end
+
+	
+	def user_attempt #Allows the user to input their guess
+		attempt_array = []
+		attempt = ""
+		attempt_int = 0
+		while attempt.length != @string_length
+			puts "Please enter a #{@string_length} digit number:"
+			attempt = gets.chomp
+			@attempt_array = attempt.split("")
+			unless attempt.length == @string_length
+				puts "Error: That is not the correct number of digits."
+			end
+		end
+	end
+
+	def attempt_check #Checks the accuracy of the guess
+		x = 0
+		hints = Hash.new
+		unless @attempt_array != @winning_array
+			puts "You win! The correct string is #{@winning_array}!"
+			exit
+		end
+		while x < @string_length
+			if @winning_array.include?(@attempt_array[x])
+				if @winning_array[x] == @attempt_array[x]
+					hints[@attempt_array[x]] = "●"
+				else
+					hints[@attempt_array[x]] = "○"
+				end
+				x += 1
+			else
+				hints[@attempt_array[x]] = "x"
+				x += 1
+			end
+		end
+		hints.each do |value, hint|
+			print "#{value}: #{hint} "
+			puts ""
+		end
 	end
 end
 
+def plays_game
+	instance = Mastermind.new
+	instance.random_number_picker
+	loop do	
+		instance.user_attempt
+		instance.attempt_check
+	end
+end
 
-instance = Mastermind.new
-puts instance.random_number_picker
+plays_game
